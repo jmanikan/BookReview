fffdc4a522dc7374d81f2bbcdea51109ebbda2d7
Juvenal Manikan <jmanikan@gmail.com>
2018-06-17 00:09:23 -0400

    Including paths for Paperclib in diff. environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-  # Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+  # Paperclip.options.merge!(:command_path => "/usr/local/bin")
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
